d35b10ca2008387aab137e5d9649e9e296d03498
MateuszZglenicki <matizglen25@wp.pl>
2022-04-07 16:31:48 +0200

    Stworzenie projektu

---
+++ src/main/java/pl/mateusz/zadanie_20/Zadanie20Application.java
@@ -0,0 +1,13 @@
+package pl.mateusz.zadanie_20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie20Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mateusz/zadanie_20/Zadanie20ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mateusz.zadanie_20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie20ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
